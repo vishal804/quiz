b441597f5b9e26fa097a759c32c74d1e75602f8e
vishal804 <vishal804>
2022-07-19 14:25:22 +0530

    refactor authentication code

--- src/pages/authenticationPage/Signin.js
+++ src/pages/authenticationPage/Signin.js
@@ -4,16 +4,14 @@ import axios from "axios";
 import { useAuth } from "../../context/auth-context";
 import { ErrorToast, SuccessToast } from "../../component";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Signin = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from || "/";
   const { authDispatch } = useAuth();
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    password: "",
-  });
+  const [showPassword, setShowPassword] = useState(false);
 
   const guestLogin = async () => {
     try {
@@ -21,6 +19,7 @@ const Signin = () => {
         email: "steveroger@gmail.com",
         password: "steveroger",
       });
+      localStorage.setItem("user", response.data.foundUser);
       localStorage.setItem("token", response.data.encodedToken);
       authDispatch({
         type: "LOGIN",
@@ -36,13 +35,10 @@ const Signin = () => {
     }
   };
 
-  const loginHandler = async (e) => {
-    e.preventDefault();
+  const loginHandler = async (values) => {
     try {
-      const response = await axios.post("/api/auth/login", {
-        email: userInfo.email,
-        password: userInfo.password,
-      });
+      const response = await axios.post("/api/auth/login", values);
+      localStorage.setItem("user", response.data.foundUser);
       localStorage.setItem("token", response.data.encodedToken);
       authDispatch({
         type: "LOGIN",
@@ -61,62 +57,97 @@ const Signin = () => {
   return (
     <>
       <div className="login-page flex flex-center">
-        <form className="form" onSubmit={loginHandler}>
-          <div className="login-heading">Login</div>
-
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            className="input"
-            type="text"
-            placeholder="Enter Email"
-            name="email"
-            required
-            value={setUserInfo.email}
-            onChange={(event) =>
-              setUserInfo({ ...userInfo, email: event.target.value })
+        <Formik
+          className="form"
+          onSubmit={(values) => {
+            loginHandler(values);
+          }}
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validate={(values) => {
+            console.log({ values });
+            const errors = {};
+            if (!values.password.trim()) {
+              errors.password = "Required";
             }
-          />
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <div className="show-hide-password">
-            <input
-              id="create-password"
-              className="input"
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              required
-              value={setUserInfo.password}
-              onChange={(event) =>
-                setUserInfo({ ...userInfo, password: event.target.value })
-              }
-            />
-            <div className="show-password" id="show-create-password">
-              show
+
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+
+            return errors;
+          }}
+        >
+          <Form className="form">
+            <div className="login-heading">Login</div>
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <Field
+                className="input"
+                type="text"
+                placeholder="Enter Email"
+                name="email"
+              />
+              <ErrorMessage name="email" component="div" />
             </div>
-          </div>
 
-          <div className="login-store flex flex-space-between">
-            <label htmlFor="store">
-              <input type="checkbox" />I accept all terms & conditions
-            </label>
-          </div>
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <div className="show-hide-password">
+                <Field
+                  id="create-password"
+                  className="input"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter Password"
+                  name="password"
+                />
+                <div
+                  className="show-password"
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  show
+                </div>
+              </div>
+              <ErrorMessage name="password" component="div" />
+            </div>
 
-          <div className="form-btn">
-            <button className="btn btn-primary btn-lg">Login</button>
-          </div>
-          <button className="btn btn-primary btn-lg" onClick={guestLogin}>
-            Guest Login
-          </button>
-          <div className="create-account flex flex-center">
-            <Link to="/signup">
-              <p>Don't have account? Create now</p>
-            </Link>
-          </div>
-        </form>
+            <div className="login-store flex flex-space-between">
+              <label htmlFor="store">
+                <input type="checkbox" />I accept all terms & conditions
+              </label>
+            </div>
+
+            <div className="form-btn">
+              <button className="btn btn-primary btn-lg" type="submit">
+                Login
+              </button>
+            </div>
+            <button
+              className="btn btn-primary btn-lg"
+              onClick={guestLogin}
+              type="button"
+            >
+              Guest Login
+            </button>
+            <div className="create-account flex flex-center">
+              <Link to="/signup">
+                <p>Don't have account? Create now</p>
+              </Link>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </>
   );
--- src/pages/authenticationPage/Signup.js
+++ src/pages/authenticationPage/Signup.js
@@ -4,162 +4,201 @@ import axios from "axios";
 import { useAuth } from "../../context/auth-context";
 import { ErrorToast, SuccessToast } from "../../component";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Signup = () => {
-  const [error, setError] = useState("");
-  const [userCredentials, setUserCredentials] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    firstName: "",
-  });
-
   const { authDispatch } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from || "/";
-  const signupHandler = async (e) => {
-    e.preventDefault();
-    if (
-      userCredentials.password.length >= 9 &&
-      userCredentials.password === userCredentials.confirmPassword
-    ) {
-      try {
-        const response = await axios.post("/api/auth/signup", {
-          email: userCredentials.email,
-          password: userCredentials.password,
-          firstName: userCredentials.firstName,
+  const [showPassword, setShowPassword] = useState(false);
+
+  const signupHandler = async (values) => {
+    try {
+      const response = await axios.post("/api/auth/signup", values);
+      if (response.status === 201) {
+        localStorage.setItem("userAuthToken", response.data.encodedToken);
+        localStorage.setItem("user", JSON.stringify(response.data.createdUser));
+        authDispatch({
+          type: "SIGNUP",
+          payload: {
+            user: response.data.createdUser,
+            token: response.data.encodedToken,
+          },
         });
-        if (response.status === 201) {
-          localStorage.setItem("userAuthToken", response.data.encodedToken);
-          localStorage.setItem(
-            "user",
-            JSON.stringify(response.data.createdUser)
-          );
-          authDispatch({
-            type: "SIGNUP",
-            payload: {
-              user: response.data.createdUser,
-              token: response.data.encodedToken,
-            },
-          });
-          navigate(from, { replace: true });
-          SuccessToast("Login successful");
-        }
-      } catch (error) {
-        ErrorToast("Invalid username and password", error);
+        navigate(from, { replace: true });
+        SuccessToast("Login successful");
       }
-    } else if (userCredentials.password.length <= 9) {
-      setError("Password required altest 9 Character");
-    } else {
-      setError("Password and Confirm Password does not match");
+    } catch (error) {
+      ErrorToast("Invalid username and password", error);
     }
   };
 
   return (
     <>
       <div className="login-page flex flex-center">
-        <form className="form" onSubmit={signupHandler}>
-          <div className="login-heading">SIGN UP</div>
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            className="input"
-            type="text"
-            placeholder="Enter Name"
-            name="name"
-            required
-            value={userCredentials.firstName}
-            onChange={(event) =>
-              setUserCredentials({
-                ...userCredentials,
-                firstName: event.target.value,
-              })
+        <Formik
+          className="form"
+          onSubmit={(values) => {
+            signupHandler(values);
+          }}
+          initialValues={{
+            confirmPassword: "",
+            firstName: "",
+            email: "",
+            password: "",
+            acceptTerms: false,
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.firstName.trim()) {
+              errors.firstName = "Required";
             }
-          />
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            className="input"
-            type="text"
-            placeholder="Enter Email"
-            name="email"
-            required
-            value={userCredentials.email}
-            onChange={(event) =>
-              setUserCredentials({
-                ...userCredentials,
-                email: event.target.value,
-              })
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
             }
-          />
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <div className="show-hide-password">
-            <input
-              id="create-password"
-              className="input"
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              required
-              value={userCredentials.password}
-              onChange={(event) =>
-                setUserCredentials({
-                  ...userCredentials,
-                  password: event.target.value,
-                })
-              }
-            />
-            <div className="show-password" id="show-create-password">
-              show
-            </div>
-          </div>
+            if (!values.password.trim()) {
+              errors.password = "Required";
+            }
+            if (!values.confirmPassword.trim()) {
+              errors.confirmPassword = "Required";
+            }
+            if (values.password !== values.confirmPassword) {
+              errors.confirmPassword =
+                "Password and confirm password doesn't match";
+            }
+            if (values.acceptTerms === false) {
+              errors.acceptTerms = "Accept Terms & Conditions is required";
+            }
+            return errors;
+          }}
+        >
+          {() => (
+            <Form className="form">
+              <div className="login-heading">SIGN UP</div>
 
-          <label htmlFor="confirmPassword" className="form-label">
-            Confirm Password
-          </label>
-          <div className="show-hide-password">
-            <input
-              id="confirm-password"
-              className="input"
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              required
-              value={userCredentials.confirmPassword}
-              onChange={(event) =>
-                setUserCredentials({
-                  ...userCredentials,
-                  confirmPassword: event.target.value,
-                })
-              }
-            />
-            <div className="show-password" id="show-confirm-password">
-              show
-            </div>
-          </div>
-          <div>{error}</div>
-          <div className="login-store flex flex-space-between">
-            <label htmlFor="store">
-              <input type="checkbox" />I accept all terms & conditions
-            </label>
-          </div>
+              <div>
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <Field
+                  className="input"
+                  type="text"
+                  placeholder="Enter Name"
+                  name="firstName"
+                />
+                <ErrorMessage
+                  className="error-message"
+                  name="firstName"
+                  component="div"
+                />
+              </div>
 
-          <div className="form-btn">
-            <button className="btn btn-primary btn-lg">
-              Create New Account
-            </button>
-          </div>
-          <div className="create-account flex flex-center">
-            <Link to="/signin">
-              <p>Already have an account</p>
-            </Link>
-          </div>
-        </form>
+              <div>
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <Field
+                  className="input"
+                  type="text"
+                  placeholder="Enter Email"
+                  name="email"
+                />
+                <ErrorMessage
+                  className="error-message"
+                  name="email"
+                  component="div"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <div className="show-hide-password">
+                  <Field
+                    id="create-password"
+                    className="input"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter Password"
+                    name="password"
+                  />
+                  <div
+                    className="show-password"
+                    id="show-create-password"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    show
+                  </div>
+                </div>
+                <ErrorMessage
+                  className="error-message"
+                  name="password"
+                  component="div"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="confirmPassword" className="form-label">
+                  Confirm Password
+                </label>
+                <div className="show-hide-password">
+                  <Field
+                    id="confirm-password"
+                    className="input"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter Password"
+                    name="confirmPassword"
+                  />
+                  <div
+                    className="show-password"
+                    id="show-create-password"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  >
+                    show
+                  </div>
+                  <ErrorMessage
+                    className="error-message"
+                    name="confirmPassword"
+                    component="div"
+                  />
+                </div>
+
+                <div className="login-store flex flex-space-between">
+                  <label htmlFor="acceptTerms">
+                    <Field type="checkbox" name="acceptTerms" />I accept all
+                    terms & conditions
+                    <ErrorMessage
+                      className="error-message"
+                      name="acceptTerms"
+                      component="div"
+                    />
+                  </label>
+                </div>
+              </div>
+
+              <div className="form-btn">
+                <button className="btn btn-primary btn-lg" type="submit">
+                  Create New Account
+                </button>
+              </div>
+
+              <div className="create-account flex flex-center">
+                <Link to="/signin">
+                  <p>Already have an account? Login here</p>
+                </Link>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </>
   );
